059eadd7bcee1265f22fadc7a44a0daa167d4cb6
Ayodeji Yekeen <ayodejiyou@gmail.com>
2024-01-05 15:19:12 +0000

    reset layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,16 @@
 import type { Metadata } from "next";
-import { Inter as FontSans } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
-import { cn } from "@/lib/utils";
 import { Toaster } from "@/components/ui/toaster";
 
-export const fontSans = FontSans({
-  subsets: ["latin"],
-  variable: "--font-sans",
-});
+
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: "SuperChat AI",
   description: "Chat with SuperChat AI",
 };
+
 export default function RootLayout({
   children,
 }: {
@@ -20,12 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body
-        className={cn(
-          "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable
-        )}
-      >
+      <body className={inter.className}>
         <main>{children}</main>
         <Toaster />
       </body>
